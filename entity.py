039e59e0c45c4bd71230e3ccd00c4af1a548ede2
ru <64133569+ruChikati@users.noreply.github.com>
2024-08-16 19:42:07 +0200

    Update entity.py

--- entity.py
+++ entity.py
@@ -1,15 +1,14 @@
 import pygame
 
 class Entity:
-    def __init__(self, x, y, w, h, name, display, img_path):
+    def __init__(self, x, y, w, h, name, anims):
         self.x = x
         self.y = y
         self.w = w
         self.h = h
         self.name = name
-        self.display = display
-        self.image = pygame.image.load(img_path)
-        pygame.transform.scale(self.image, (self.w, self.h))
+        self.anims = anims
+        self.action = "idle"
 
-    def update(self, dt):
-        self.display.blit(self.image, (self.x, self.y))
+    def update(self, surf, dt):
+        surf.blit(self.anims[self.action].play(dt))
