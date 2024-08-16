ec1e5417e875bc81829411789a6ba60011207692
Aadith Yadav <aadith.ygalu@gmail.com>
2024-08-16 19:50:55 +0200

    Added wallpaper

---
+++ wallpaper.py
@@ -0,0 +1,33 @@
+import pygame
+
+
+class WallpaperTile:
+    def __init__(self, surface, x, y, width, height, tile_size, filename):
+        self.screen = surface
+        self.rect = pygame.Rect(x, y, width, height)
+        self.image = pygame.image.load(filename)
+
+        self.z = tile_size
+
+        self.w_tiles = []
+
+    def load_tiles(self):
+        n = self.rect.width // self.z
+        m = self.rect.height // self.z
+
+        for i in range(n):
+            temp_arr = []
+            for j in range(m):
+                temp_arr.append(
+                    self.image.subsurface(
+                        i * self.rect.width, j * self.rect.height, self.rect.width, self.rect.height
+                    )
+                )
+
+            self.w_tiles.append(temp_arr)
+
+    def draw(self, surface_coordinate):
+        for i in range(len(self.image)):
+            for j in range(len(self.image[i])):
+                self.screen.blit(self.screen, (i*width + 1, j*height + 1))
+
