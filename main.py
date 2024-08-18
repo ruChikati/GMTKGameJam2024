3a6dcffda18273e5a27fe19888544a386d7cac5e
ru <rushil.cn@gmail.com>
2024-08-18 17:14:25 +0200

    added player movement and theta input handling

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 
 import pygame
 
+import input
 from anim import Animation
 from entity import Entity
 from wallpaper import Wallpaper
@@ -11,14 +12,17 @@ from wallpaper import Wallpaper
 display = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Drawn to Scale")
 
-screen = pygame.Surface((640, 360))
+inputs = input.Input()
+screen = pygame.Surface((320, 180))
 clock = pygame.time.Clock()
 dt = 1.0
 last_time = time.time()
 
 player = Entity(
-    0, 0, 16, 16, "player", {"idle": Animation(f"anims{os.sep}player;idle")}, screen
+    152, 119, 16, 16, "player", {"idle": Animation(f"anims{os.sep}player;idle")}, screen
 )
+
+speed = 1
 
 tile_imgs = {}
 for file in os.listdir("tiles"):
@@ -33,48 +37,38 @@ tile_selected = (0, 0)
 
 while True:
 
-    dt = (time.time() - last_time) * 60 * 10
+    dt = time.time() - last_time
     last_time = time.time()
-    # print(dt)
     screen.fill((255, 248, 231))
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygame.Vector2(pygame.mouse.get_pos())
-            if pygame.mouse.get_pressed()[0]:
-                for i in range(16):
-                    for j in range(16):
-                        if (wallpaper.w_tiles[i][j].rect.x - 15 < pos.x - scroll.x < wallpaper.w_tiles[i][j].rect.x + 15)\
-                                and (wallpaper.w_tiles[i][j].rect.y - 15 < pos.y + scroll.y < wallpaper.w_tiles[i][j].rect.y + 15):
-                            print("Tile selected:", i, j)
-                            tile_selected = (i, j)
+    for event in inputs.get():
+        match event.type:
+            case input.QUIT:
+                pygame.quit()
+                sys.exit()
+            case input.KEYDOWN2:
+                match event.key:
+                    case input.ESCAPE:
+                        pygame.quit()
+                        sys.exit()
+            case input.KEYHOLD:
+                match event.key:
+                    case input.W:
+                        player.move(pygame.Vector2(0, -speed))
+                        scroll.y += -speed
+                    case input.A:
+                        player.move(pygame.Vector2(-speed, 0))
+                        scroll.x += -speed
+                    case input.S:
+                        player.move(pygame.Vector2(0, speed))
+                        scroll.y += speed
+                    case input.D:
+                        player.move(pygame.Vector2(speed, 0))
+                        scroll.x += speed
 
-        if event.type == pygame.MOUSEWHEEL:
-            scroll.x -= 5 * event.x
-            scroll.y += 5 * event.y
-            if 640 < scroll.x: scroll.x = 640
-            if 0 > scroll.x: scroll.x = 0
-            if -360 > scroll.y: scroll.y = -360
-            if 0 < scroll.y: scroll.y = 0
-
-    # print(scroll)
     wallpaper.draw(scroll)
 
-    '''pygame.draw.rect(screen, (255, 0, 0), (0, 360, 32, 32))
-    pygame.draw.rect(screen, (0, 255, 0), (32, 360, 32, 32))
-    pygame.draw.rect(screen, (0, 0, 255), (64, 360, 32, 32))
-    pygame.draw.rect(screen, (255, 255, 0), (96, 360, 32, 32))
-    pygame.draw.rect(screen, (0, 255, 255), (128, 360, 32, 32))
-    pygame.draw.rect(screen, (255, 0, 255), (160, 360, 32, 32))
-    pygame.draw.rect(screen, (255, 255, 255), (192, 360, 32, 32))
-    pygame.draw.rect(screen, (0, 0, 0), (224, 360, 32, 32))'''
-
-    pygame.draw.rect(screen, (255, 0, 0), (tile_selected[0] * 32 + scroll.x - 16, tile_selected[1] * 32 + scroll.y - 16, 32, 32), 3)
-
-    player.update(dt)
+    player.update(dt, scroll)
     display.blit(pygame.transform.scale(screen, display.get_size()), (0, 0))
     pygame.display.flip()
     clock.tick()
