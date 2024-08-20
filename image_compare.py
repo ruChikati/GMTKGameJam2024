bcfaeb811519dbc9e1e8e38ec2360e9b59cb9560
Anirudh <yamunan.bitset@gmail.com>
2024-08-20 11:27:38 +0200

    fixed score

--- image_compare.py
+++ image_compare.py
@@ -1,15 +1,12 @@
-import io, os, random
-
-import imgcompare, math, operator
+import os
+from imgcompare import image_diff_percent
 from PIL import Image
 
 
 def score(artwork, screenshot):
     size = 500, 500
-    img2 = Image.open(screenshot)
-    img2.thumbnail(size, Image.Resampling.LANCZOS)
-    img2.save(screenshot, "png")
     img1 = Image.open(f"artworks{os.sep}{artwork}").convert('RGB')
-    img1.thumbnail(size, Image.Resampling.LANCZOS)
-    img1.save(f"artworks{os.sep}{artwork}", "png")
-    return imgcompare.image_diff_percent(img1, img2)
+    img1.resize(size)
+    img2 = Image.open(screenshot)
+    img2.resize(size)
+    return image_diff_percent(img1.resize(size), img2.resize(size))
